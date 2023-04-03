53c80a30695ff03d1cbcc47417c384d58b10053d
ImGili <18017158729@163.com>
2023-04-03 12:24:40 +0800

    ✨ feat: 改为中文

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: '极客盖博客',
+  author: '极客盖',
+  headerTitle: '极客盖博客',
+  description: '技术生活博客',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
